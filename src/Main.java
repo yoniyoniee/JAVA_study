b5ea211e828cd7a1d3d58c8cc6e87c83c45c02ca
yoniyoniee <shy7235@naver.com>
2024-03-25 14:17:00 +0900

    first project

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    // JDK
+    // compiler : .java -> .class
+    // JRE
+    // JDB: 디버깅
+
+    public static void main(String[] args) {
+        System.out.println("Our First Project! :");
+    }
+}
