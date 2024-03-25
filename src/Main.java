2ad66599ced5791bf020cde866f45695b931c958
yoniyoniee <shy7235@naver.com>
2024-03-25 15:41:11 +0900

    main.md readme

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,9 @@ public class Main {
     // JRE
     // JDB: 디버깅
 
+    // main 메소드
+    // 자바 프로젝트는 제일 먼저 클래스의 main 메소드를 실행시킨다.
+
     public static void main(String[] args) {
         System.out.println("Our First Project! :");
 
